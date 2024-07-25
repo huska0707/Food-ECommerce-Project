22672b5ab392e9a15070d635a905373ae4910d7e
huska0707 <hussaink001haled@gmail.com>
2024-07-24 19:38:41 -0500

    Add functions with cart to context

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { commerce } from "../lib/commerce";
 
 const context = React.createContext();
 
 const ContextProvider = (props) => {
+  const [cart, setCart] = useState({});
+
+  const [Token, setToken] = useState();
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  let fetchCart = async () => {
+    let response = await commerce.cart.retrieve();
+    setCart(response);
+  };
+
+  let AddToCart = async (id, quan) => {
+    let response = await commerce.cart.add(id, quan);
+    setCart(response);
+  };
+
+  let UpdateCartQuantity = async (id, quantity) => {
+    let response = await commerce.cart.update(id, { quantity });
+    setCart(response);
+  };
+
+  let refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+    setCart(newCart);
+  };
+
   return (
     <>
-      <context.Provider>{props.children}</context.Provider>
+      <context.Provider
+        value={{
+          cart,
+          AddToCart,
+          UpdateCartQuantity,
+          refreshCart,
+        }}
+      >
+        {props.children}
+      </context.Provider>
     </>
   );
 };
 
-export default ContextProvider;
+export { context, ContextProvider };
