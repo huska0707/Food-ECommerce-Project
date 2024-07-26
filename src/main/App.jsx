1a8ead694096ea137598ff9ad3440600751361f4
huska0707 <hussaink001haled@gmail.com>
2024-07-25 20:37:45 -0500

    import React from "react";

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react";
 import Home from "../pages/Home";
+import Menu from "../pages/Menu";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </>
   );
