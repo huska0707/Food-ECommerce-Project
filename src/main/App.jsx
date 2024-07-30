90739794a66e48be0da0afda8344b7dc2143ffae
huska0707 <hussaink001haled@gmail.com>
2024-07-30 15:17:14 -0500

    Add cart to router

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react";
 import Home from "../pages/Home";
 import Menu from "../pages/Menu";
+import Cart from "../pages/Cart";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </>
   );
