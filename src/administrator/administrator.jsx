e45a981d62a6a614b1a8f12c07cffa116dc65168
huska0707 <hussaink001haled@gmail.com>
2024-07-30 15:21:04 -0500

    Add Review page

--- src/administrator/administrator.jsx
+++ src/administrator/administrator.jsx
@@ -4,10 +4,10 @@ const Admin = () => {
   return (
     <a
       className="w-full text-zinc-500 font-mono no-underline hover:text-zinc-400 flex items-center justify-center text-sm font-extralight cursor-default md:cursor-pointer"
-      href="https://www.github.com/sumyat-aung"
+      href="https://www.github.com/huska0707"
       target="_blank"
     >
-      Designed & built by - Su Myat Aung
+      Designed & built by huska
     </a>
   );
 };
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Menu from "../pages/Menu";
 import Cart from "../pages/Cart";
 import ContactUs from "../pages/Contact-us";
+import Review from "../pages/Review";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </>
   );
