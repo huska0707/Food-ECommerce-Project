d4d36c95bd3473f879924a5d710a8d96362c7155
huska0707 <hussaink001haled@gmail.com>
2024-07-30 15:14:54 -0500

    EmptyCart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,23 +4,29 @@ import Nav from "../components/header/Nav";
 import CartButtons from "../components/cart/CartButtons";
 import { context } from "../context/context";
 import Admin from "../administrator/administrator";
+import EmptyCart from "../components/cart/EmptyCart";
 
 const Cart = () => {
   const contextConsumer = useContext(context);
   const { cart, EmptyCartHandle } = contextConsumer;
 
+  let { line_items } = cart;
+
   return (
     <>
       <Nav />
-
-      <div className="flex justify-center flex-col my-14 items-center">
-        <div className="flex justify-center items-center flex-wrap">
-          <CartButtons EmptyCartHandle={EmptyCartHandle} cart={cart} />
+      {line_items && line_items.length <= 0 ? (
+        <EmptyCart />
+      ) : (
+        <div className="flex justify-center flex-col my-14 items-center">
+          <div className="flex justify-center items-center flex-wrap">
+            <CartButtons EmptyCartHandle={EmptyCartHandle} cart={cart} />
+          </div>
+          <div className="mt-10 -mb-10">
+            <Admin />
+          </div>
         </div>
-        <div className="mt-10 -mb-10">
-          <Admin />
-        </div>
-      </div>
+      )}
     </>
   );
 };
