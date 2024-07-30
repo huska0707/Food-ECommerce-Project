1a8abd76b37b00108005e565ebf4595221336b8a
huska0707 <hussaink001haled@gmail.com>
2024-07-30 15:18:05 -0500

    Contact us page

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react";
 import Home from "../pages/Home";
 import Menu from "../pages/Menu";
 import Cart from "../pages/Cart";
+import ContactUs from "../pages/Contact-us";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
     </>
   );
