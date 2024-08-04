5b9b213344c8e181e41515bc63474aad6e72ca72
huska0707 <hussaink001haled@gmail.com>
2024-08-04 10:13:11 -0500

    Part:Go Back to Cart

--- src/checkout/checkout.jsx
+++ src/checkout/checkout.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from "react";
 import { context } from "../context/context";
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
   const contextConsumer = useContext(context);
   const { Token, cart, refreshCart, setToken } = contextConsumer;
   const steps = ["Shipping Address", "Payment Details"];
   const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
 
   const [AddressFormData, setAddressFormData] = useState({
     fname: "",
@@ -19,7 +21,15 @@ const Checkout = () => {
     shippingOption: "",
   });
 
-  const [complete, setComplete] = useState(false);
+  const { fname, lname, address, email, city, postalCode, country } =
+    AddressFormData;
+
+  let enableButton =
+    fname && lname && address && email && city && postalCode && country;
+
+  const NextBtnHandle = () => {
+    setCurrentStep((prev) => prev + 1);
+  };
 
   return (
     <div className="flex justify-center items-center py-10 ">
@@ -45,6 +55,24 @@ const Checkout = () => {
             </div>
           ))}
         </div>
+
+        {!complete && currentStep !== steps.length && (
+          <div className="w-full md:flex-row flex-col-reverse flex md:justify-between justify-center items-center px-5 md:h-[100px] h-[150px]">
+            <Link
+              to={"/cart"}
+              className="px-12 py-2 text-gray-50 bg-blue-900 hover:bg-blue-800  text-xl border border-yel rounded-md font-mono md:mt-0 mt-5"
+            >
+              Go Back To Cart
+            </Link>
+            <button
+              className="px-12 py-2 text-gray-50 bg-blue-900 hover:bg-blue-800  text-xl border border-yel rounded-md font-mono disabled:bg-blue-800 disabled:cursor-not-allowed "
+              onClick={NextBtnHandle}
+              disabled={!enableButton}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
